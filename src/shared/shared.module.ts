afa993717d1b26db61bf937e6e98968daa0a6c4b
Williams <wilfre_padilla@hotmail.com>
2022-09-22 14:44:37 -0400

    Feat: app button Feat: primary and secondary types

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from "@angular/core";
 import { IconComponent } from "./components/icon/icon.component";
 import { InputSearchDirective } from './directives/input-search.directive';
+import { ButtonComponent } from './components/button/button.component';
+import { CommonModule } from "@angular/common";
 
 @NgModule({
   declarations: [
     IconComponent,
-    InputSearchDirective
+    InputSearchDirective,
+    ButtonComponent
   ],
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   providers: [],
   exports: [
     IconComponent,
-    InputSearchDirective
+    InputSearchDirective,
+    ButtonComponent
   ]
 })
 export class SharedModule { }
